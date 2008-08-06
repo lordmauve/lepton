fb2f90f94441d7f6d7468126a8530bb2f971e2db
casey.duncan <casey.duncan@localhost>
2008-08-06 23:41:16 +0000

    - Add license headers - Clean up billboard example a bit

--- lepton/vector.c
+++ lepton/vector.c
@@ -1,3 +1,15 @@
+/****************************************************************************
+*
+* Copyright (c) 2008 by Casey Duncan and contributors
+* All Rights Reserved.
+*
+* This software is subject to the provisions of the MIT License
+* A copy of the license should accompany this distribution.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+*
+****************************************************************************/
 /* Vector and color support
  *
  * $Id$ 
