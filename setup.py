bf319263b71c0762f46339c08b604a924b73b429
casey.duncan <casey.duncan@localhost>
2008-08-17 01:10:25 +0000

    Bump version for 0.5a release and update changelist

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ compile_args = [] # disable compile args for now
 
 setup(
 	name='lepton',
-    version='0.4a', # *** REMEMBER TO UPDATE __init__.py ***
+    version='0.5a', # *** REMEMBER TO UPDATE __init__.py ***
 	description='Lepton: A high-performance, pluggable particle engine and API for Python',
 	long_description='''\
 Lepton is designed to make complex and beautiful particle effects possible,
